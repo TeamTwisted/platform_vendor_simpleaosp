ca1f82105703bea0fc350fd16f1c5c1ef260e56e
aow1980 <wilkes.allen@gmail.com>
2015-07-28 18:20:51 -0400

    Let's Get Twisted with Mako

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twisted_hammerhead.mk \
     $(LOCAL_DIR)/twisted_shamu.mk \
+    $(LOCAL_DIR)/twisted_mako.mk
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 add_lunch_combo twisted_hammerhead-user
 add_lunch_combo twisted_shamu-user
-
+add_lunch_combo twisted_mako-user
