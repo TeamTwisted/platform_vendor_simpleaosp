b0adf9c3dc6fe2966590940b5023d216a9bbd244
aow1980 <wilkes.allen@gmail.com>
2015-08-08 17:57:03 -0400

    Update fingerprints

--- products/twisted_mako.mk
+++ products/twisted_mako.mk
@@ -14,5 +14,5 @@ PRODUCT_MANUFACTURER := LGE
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="occam" \
-    BUILD_FINGERPRINT="google/occam/mako:5.1.1/LVY48C/1914015:user/release-keys" \
-    PRIVATE_BUILD_DESC="occam-user 5.1.1 LVY48C 1914015 release-keys"
+    BUILD_FINGERPRINT="google/occam/mako:5.1.1/LMY48I/2074855:user/release-keys" \
+    PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48I 2074855 release-keys"
