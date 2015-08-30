ujwalp15/android_vendor_SaberMod

[armv7-a] Add prebuilt mozilla firefox browser compiled with sm-4.9 by @frap129
Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

prebuilts/system/lib/armeabi-v7a/arm.mk
@@ -1,0 +1,4 @@
+PRODUCT_COPY_FILES += \
+    vendor/sm/prebuilts/system/lib/armeabi-v7a/libmozglue.so:system/lib/libmozglue.so \
+    vendor/sm/prebuilts/system/lib/armeabi-v7a/libplugin-container.so:system/lib/libplugin-container.so \
+    vendor/sm/prebuilts/system/lib/armeabi-v7a/libplugin-container-pie.so:system/lib/libplugin-container-pie.so

TeamTwisted/platform_vendor_simpleaosp

Conflicts:
	device/sm_devices.mk

prebuilts/system/lib/armeabi-v7a/arm.mk
@@ -1,0 +1,4 @@
+PRODUCT_COPY_FILES += \
+    vendor/twisted/prebuilts/system/lib/armeabi-v7a/libmozglue.so:system/lib/libmozglue.so \
+    vendor/twisted/prebuilts/system/lib/armeabi-v7a/libplugin-container.so:system/lib/libplugin-container.so \
+    vendor/twisted/prebuilts/system/lib/armeabi-v7a/libplugin-container-pie.so:system/lib/libplugin-container-pie.so
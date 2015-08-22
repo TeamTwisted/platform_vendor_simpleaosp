a6fb77ed3d58b6136d923442469631518a5f922b
Paul Beeler <pbeeler80@gmail.com>
2015-08-22 18:22:56 -0400

    [armv7-a] Add prebuilt mozilla firefox browser compiled with sm-4.9 by @frap129
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Conflicts:
    	device/sm_devices.mk

---
+++ prebuilts/system/lib/armeabi-v7a/arm.mk
@@ -0,0 +1,4 @@
+PRODUCT_COPY_FILES += \
+    vendor/twisted/prebuilts/system/lib/armeabi-v7a/libmozglue.so:system/lib/libmozglue.so \
+    vendor/twisted/prebuilts/system/lib/armeabi-v7a/libplugin-container.so:system/lib/libplugin-container.so \
+    vendor/twisted/prebuilts/system/lib/armeabi-v7a/libplugin-container-pie.so:system/lib/libplugin-container-pie.so
