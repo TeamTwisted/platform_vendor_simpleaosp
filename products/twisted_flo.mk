7dcae4dfb4ea1de9ecc68911f20033ff9939e51f
aow1980 <wilkes.allen@gmail.com>
2015-09-20 11:30:04 -0400

    Update devices to LMY48M

--- products/twisted_flo.mk
+++ products/twisted_flo.mk
@@ -14,5 +14,5 @@ PRODUCT_MANUFACTURER := asus
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="razor" \
-    BUILD_FINGERPRINT="google/razor/flo:5.1.1/LVY48C/1914015:user/release-keys" \
-    PRIVATE_BUILD_DESC="razor-user 5.1.1 LVY48C 1914015 release-keys"
+    BUILD_FINGERPRINT="google/razor/flo:5.1.1/LMY48M/2167285:user/release-keys" \
+    PRIVATE_BUILD_DESC="razor-user 5.1.1 LMY48M 2167285 release-keys"
