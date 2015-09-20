52eabbe444b7efff561c233a4762156c42c8c76e
aow1980 <wilkes.allen@gmail.com>
2015-09-20 11:31:41 -0400

    Updating Sepolicy

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -7,11 +7,13 @@ BOARD_SEPOLICY_DIRS += \
     vendor/twisted/sepolicy
 
 BOARD_SEPOLICY_UNION += \
+file.te \
 mediaserver.te \
 service_contexts \
 shell.te \
 sysinit.te \
 system.te \
+system_server.te \
 userinit.te\
 file_contexts 
 
